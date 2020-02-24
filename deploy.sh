d4cc0e66603d2046cf83d8cf59689da5ef629ac1
Troy Kelly <troy@troykelly.com>
2020-02-24 11:26:10 +1100

    Added GA logging

--- deploy.sh
+++ deploy.sh
@@ -25,5 +25,5 @@ gcloud services enable --project=$(read_var PROJECT_ID) cloudbuild.googleapis.co
 echo "Submitting $(read_var SERVICE_NAME) build in $(read_var PROJECT_ID)"
 gcloud builds submit --config=cloudbuild.yaml \
     --project=$(read_var PROJECT_ID) \
-    --substitutions=_SERVICE_NAME=$(read_var SERVICE_NAME),_REGION=$(read_var REGION),_REDIRECT_TYPE=$(read_var REDIRECT_TYPE),_REDIRECT_TARGET=$(read_var REDIRECT_TARGET),COMMIT_SHA=manual${TS} \
+    --substitutions=_SERVICE_NAME=$(read_var SERVICE_NAME),_REGION=$(read_var REGION),_REDIRECT_TYPE=$(read_var REDIRECT_TYPE),_REDIRECT_TARGET=$(read_var REDIRECT_TARGET),_GA=$(read_var GA),COMMIT_SHA=manual${TS} \
     .
--- redirect.py
+++ redirect.py
@@ -1,23 +1,60 @@
 import os
 import logging
 
-from flask import Flask,redirect
+from flask import Flask, redirect, request, session
+from pyga.requests import Tracker, Page, Session, Visitor
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 
-redirect_type = int(os.environ.get('REDIRECT_TYPE', '302'))
-redirect_target = os.environ.get('REDIRECT_TARGET', 'https://www.pioneera.com/')
-listening_port=int(os.environ.get('PORT', 8080))
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
-logging.debug('Adding redirection handling to target %s with code %d',redirect_target,redirect_type)
+
+redirect_type = int(os.environ.get('REDIRECT_TYPE', '302'))
+redirect_target = os.environ.get(
+    'REDIRECT_TARGET', 'https://www.pioneera.com/')
+google_analytics = os.environ.get('GA', None)
+listening_port = int(os.environ.get('PORT', 8080))
+
+app.logger.info('Adding redirection handling to target %s with code %d',
+                redirect_target, redirect_type)
+
+if google_analytics:
+    app.logger.info('Logging to Google Analytics as %s',
+                    google_analytics)
+
+
+def log_traffic(base_url, ip):
+    if not google_analytics:
+        return
+    url = urlparse(base_url)
+    app.logger.info(
+        'Logging GA traffic from %s to host %s with page %s', ip, url.hostname, url.path)
+    tracker = Tracker(google_analytics, url.hostname)
+    pyga_visitor = Visitor()
+    pyga_visitor.ip_address = ip
+    pyga_session = Session()
+    pyga_page = Page(url.path)
+    try:
+        tracker.track_pageview(pyga_page, pyga_session, pyga_visitor)
+    except:
+        app.logger.error('Unable to connect to analytics')
+
 
 @app.route('/')
 def root():
+    log_traffic(request.base_url, request.remote_addr)
     return redirect(redirect_target, code=redirect_type)
+
 
 @app.route('/<path:page>')
 def anypage(page):
+    log_traffic(request.base_url, request.remote_addr)
     return redirect('{new_url}#{page}'.format(page=page, new_url=redirect_target), code=redirect_type)
 
+
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=listening_port)
+    app.run(debug=True, host='0.0.0.0', port=listening_port)
